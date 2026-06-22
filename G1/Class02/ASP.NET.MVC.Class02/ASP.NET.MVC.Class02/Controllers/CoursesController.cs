a94301d01f8eb2b55bba7788da22f7f24c1908f3
Danilo Borozan <daniloborozan07@gmail.com>
2026-06-22 18:56:02 +0200

    add

---
+++ G1/Class02/ASP.NET.MVC.Class02/ASP.NET.MVC.Class02/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.MVC.Class02.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ G1/Class02/ASP.NET.MVC.Class02/ASP.NET.MVC.Class02/Controllers/StudentController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.MVC.Class02.Controllers
+{
+    public class StudentController : Controller
+    {
+    }
+}
