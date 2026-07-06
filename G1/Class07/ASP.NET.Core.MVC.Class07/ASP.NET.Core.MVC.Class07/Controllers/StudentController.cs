cde084bf494c6e85309345300fabc6586264b260
Danilo Borozan <daniloborozan07@gmail.com>
2026-07-06 20:05:56 +0200

    add

---
+++ G1/Class07/ASP.NET.Core.MVC.Class07/ASP.NET.Core.MVC.Class07/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using ASP.NET.Core.MVC.Class07.Database;
+using ASP.NET.Core.MVC.Class07.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NET.Core.MVC.Class07.Helpers;
+
+namespace ASP.NET.Core.MVC.Class07.Controllers
+{
+    [Route("students")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<StudentVM> students = StaticDb.Students.Select(x =>
+            Mapper.MapToStudentVM(x)
+            ).ToList();
+
+            return View(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById([FromRoute]int id)
+        {
+            var student = StaticDb.Students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            var studentVM = Mapper.MapToStudentVM(student);
+            return View("StudentDetails", studentVM);
+        }
+    }
+}
---
+++ G1/Class07/ASP.NET.Core.MVC.Class07/ASP.NET.Core.MVC.Class07/Helpers/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET.Core.MVC.Class07.Models.Domain;
+using ASP.NET.Core.MVC.Class07.Models.ViewModels;
+
+namespace ASP.NET.Core.MVC.Class07.Helpers
+{
+    public static class Mapper
+    {
+        public static StudentVM MapToStudentVM(Student student)
+        {
+            return new StudentVM
+            {
+                Id = student.Id,
+                FullName = student.GetFullName(),
+                Age = DateTime.Now.Year - student.DateOfBirth.Year,
+                Email = student.Email
+            };
+        }
+    }
+}
