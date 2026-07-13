aecaeb3cd6a456f9f407f66e79dd9d3d7edcba3f
Danilo Borozan <daniloborozan07@gmail.com>
2026-07-13 20:45:13 +0200

    add

--- G1/Class09/Avenga.ASP.NET.CORE.MVC.Database/Avenga.ASP.NET.CORE.MVC.Database/Controllers/StudentController.cs
+++ G1/Class09/Avenga.ASP.NET.CORE.MVC.Database/Avenga.ASP.NET.CORE.MVC.Database/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using Avenga.ASP.NET.CORE.MVC.Database.DataAccess;
 using Avenga.ASP.NET.CORE.MVC.Database.Models.Domains;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Avenga.ASP.NET.CORE.MVC.Database.Controllers
 {
@@ -15,8 +16,24 @@ namespace Avenga.ASP.NET.CORE.MVC.Database.Controllers
 
         public IActionResult Index()
         {
+
+            //SELECET * FROM STUDENTS
             List<Student> students = _context.Students.ToList();
             return View(students);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            var courses = _context.Courses.ToList();
+            ViewBag.Courses = new SelectList(courses,"Id", "Name");
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromForm]Student student)
+        {
+            return View(student);
         }
     }
 }
