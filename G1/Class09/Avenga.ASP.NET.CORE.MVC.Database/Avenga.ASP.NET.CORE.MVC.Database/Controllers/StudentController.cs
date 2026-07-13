a3534c2c1d6559b5a1c1797614fd73cf93e764a9
Danilo Borozan <daniloborozan07@gmail.com>
2026-07-13 20:28:16 +0200

    add

---
+++ G1/Class09/Avenga.ASP.NET.CORE.MVC.Database/Avenga.ASP.NET.CORE.MVC.Database/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Avenga.ASP.NET.CORE.MVC.Database.DataAccess;
+using Avenga.ASP.NET.CORE.MVC.Database.Models.Domains;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avenga.ASP.NET.CORE.MVC.Database.Controllers
+{
+    [Route("students")]
+    public class StudentController : Controller
+    {
+        private readonly DemoDbContext _context;
+        public StudentController(DemoDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Student> students = _context.Students.ToList();
+            return View(students);
+        }
+    }
+}
